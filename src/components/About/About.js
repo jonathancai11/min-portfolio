91c412372c7f97422b1e49b72961a7a28c4ca55d
Jonathan Cai <caimjonathan@gmail.com>
2020-07-12 12:10:45 -0700

    new image and project changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import pdf from '../../files/simple_resume.pdf';
+import pic from '../../files/me.jpg'
+import './About.css'
 
 export default function About(props) {
     return (
-        <div>
-            <p>Hi, I'm Jonathan. I currently study computer science at Rice University.</p>
-            <p>This website is for random stuff related to my interests, work, and projects.</p>
-            {/* <p>Clear inspiration from <a target="_blank" rel="noopener noreferrer" href={"https://patrickcollison.com/"}>Patrick Collison</a>.</p> */}
-
-            <p>If you're a no-BS kind of person, or a recruiter, here's <a target="_blank" rel="noopener noreferrer" href={pdf}>my resume</a> (no guarantees on how up-to-date it is).</p>
-            <p>Website last updated 6/21/20</p>
+        <div className="AboutPage">
+            <img src={pic} className="Picture"/>
+            <div>
+                <p>Hi, I'm Jonathan. I currently study computer science at rice university.</p>
+                <p>This website is for random stuff related to my interests, work, and projects.</p>
+                <p>If you're a recruiter, or just a no-bs kind of person, here's <a target="_blank" rel="noopener noreferrer" href={pdf}>my resume</a> (no guarantees on how up-to-date it is).</p>
+                <p>Website last updated 6/21/20</p>
+            </div>
         </div>
     )
 }
