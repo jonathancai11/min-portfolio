534aa908ece9b870f1cc42a114aff4ff7105dd98
Jonathan Cai <caimjonathan@gmail.com>
2020-08-19 12:06:31 -0700

    add resume to about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import pdf from '../../files/jonathan_cai_resume.pdf';
 import pic from '../../files/me.jpg'
 import './About.css'
 
@@ -10,6 +11,7 @@ export default function About(props) {
                 <p>Hi, I'm Jonathan. Former sperm cell, future space dust.</p> 
                 <p>I currently study computer science at rice university.</p>
                 <p>This website is for anything related to my interests, work, and projects.</p>
+                <p>If you're a recruiter, or just interested in that kind of thing, here's <a target="_blank" rel="noopener noreferrer" href={pdf}>my resume</a> (no guarantees on how up to date it is).</p>
                 <i>Website last updated 8/17/20</i>
             </div>
         </div>
