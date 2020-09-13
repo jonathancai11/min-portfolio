7cf150f2b9f01e52295d9bbd2d37069efc739bd2
Jonathan Cai <caimjonathan@gmail.com>
2020-09-12 23:16:40 -0700

    sidebar changes

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,12 +5,12 @@ export default function Sidebar() {
 
     return (
     <div className="sidebar">
-        <a href="/#about">about</a>
-        <a href="/#contact">contact</a>
-        <a href="/#work">work</a>
-        <a href="/#projects">projects</a>
-        <a href="/#read">read</a>
-        <a href="http://blog.jonathancai.com/blog/">blog</a>
+        <b><a href="/#about">about</a></b>
+        <b><a href="/#contact">contact</a></b>
+        <b><a href="/#work">work</a></b>
+        <b><a href="/#projects">projects</a></b>
+        <b><a href="/#read">read</a></b>
+        <b><a href="http://blog.jonathancai.com/blog/">blog</a></b>
     </div>
     )
 }
